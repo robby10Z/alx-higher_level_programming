5f70ced459bc2d362906a60d97429ffb1b315534
robby10Z <robertnashiefd@gmail.com>
2022-06-21 06:31:50 -0700

    come through

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 """ this module defines a empty class square """
 
 
--- 0x06-python-classes/diss.py
+++ 0x06-python-classes/diss.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+!/usr/bin/python3
 import dis
 
 t = __import__('103-magic_class')
