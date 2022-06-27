b2f44d99013f670d74dd28290da27cc877bd92d9
robby10Z <robertnashiefd@gmail.com>
2022-06-27 13:58:16 -0700

    pYTHON IS THE WAY

--- 0x07-python-test_driven_development/tests/3-main.py
+++ 0x07-python-test_driven_development/tests/3-main.py
@@ -1,4 +1,4 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 say_my_name = __import__('3-say_my_name').say_my_name
 
 say_my_name("John", "Smith")
--- 0x07-python-test_driven_development/tests/4-main.py
+++ 0x07-python-test_driven_development/tests/4-main.py
@@ -1,4 +1,4 @@
-!/usr/bin/python3
+#!/usr/bin/python3
 print_square = __import__('4-print_square').print_square
 
 print_square(4)
